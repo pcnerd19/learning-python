3aca43ad3a8d909bc124da9d35696a9f0cc2fa8e
Zane Barthel <zane@pcnerd19.com>
2017-09-24 18:14:40 -0400

    Added script 1

---
+++ test.py
@@ -0,0 +1,6 @@
+print("Hello, and welcome to a test interactive experience.")
+print("Before we get started, I need to ask you a few questions.")
+print("What is your name?")
+name = input("Answer: ")
+print("Alright, now what's your gender? It's kind of hard tell.")
+gender = input("Answer: ")
